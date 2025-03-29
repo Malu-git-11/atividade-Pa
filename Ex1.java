c4d0dfa733df10deeb96a7f0fb17715c1874da69
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-03-28 21:51:22 -0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package recuperação;
-
-import static java.lang.System.console;
-
-/**
- *
- * @author Admin
- */
 public class Recuperação {
 
     /**
